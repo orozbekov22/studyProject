46da6166686d33217d2c7148fd9eeb77954a0163
Copybookss <orzbekov22@gmail.com>
2023-04-17 11:43:41 +0600

    Init commit

---
+++ src/main/java/com/example/rentcarwebapp/RentCarWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.rentcarwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarWebAppApplication.class, args);
+        //new project
+    }
+
+}
---
+++ src/test/java/com/example/rentcarwebapp/RentCarWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentcarwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
